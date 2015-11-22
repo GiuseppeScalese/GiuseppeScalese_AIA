07cab9fe052124fed4443caddb2866479541b851
Beppe <scalese.giuseppe@gmail.com>
2015-11-22 23:35:03 +0000

    First commit

---
+++ INSTRUCTIONS_READ_ME.txt
@@ -0,0 +1,9 @@
+Run the index.html page and check the test out.
+
+The test has been implemented both on Google Chrome and Firefox. It is fully responsive - mobile version availale.
+Also, the test is IE compatible. IE9(included) onwards.
+
+I've used SASS, HTML5 and GRUNT as task runner.
+
+From the terminal, run "GRUNT" in order to compile the whole project running all tasks.
+Otherwise run each task singularly.
---
+++ js/function.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+  //Click handler - performs the mobile menu toggle
+  $("#mobileMenu > span").click(function(){
+    $("#desktopNav").toggle();
+  });
+
+  //The toggle method adds a style attribute to the navigation element by default. 
+  //To fix this we need to remove the "style" attribute using the removeAttr method when we are not in mobile view.
+  $(window).resize(function(){
+    if(window.innerWidth > 480) {
+      $("#desktopNav").removeAttr("style");
+    }
+  });
+
+});
+
